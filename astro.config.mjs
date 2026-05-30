fadc78291b68a9d87841a74e5ca30f1fbe772d62
AlanStaline <staline@163.com>
2026-05-30 21:38:48 +0800

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://staline.online',
   //base: '/staline.online',
+  trailingSlash: 'always',  // 让 Astro 处理斜杠重定向
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [],
