721e00a8308af1f461bc375a4fcce61452dbfc5c
AlanStaline <staline@163.com>
2026-05-30 17:36:17 +0800

    fix: 更新站点 URL 至 https://staline.online

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://github.com/AlanStaline',
+  site: 'https://staline.online',
   base: '/staline.online',
   integrations: [tailwind()],
   markdown: {
