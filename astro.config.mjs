1bb76840e176073efcbc6607020336df03384c95
AlanStaline <staline@163.com>
2026-05-30 18:11:52 +0800

    fix: 注释掉 base URL 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://staline.online',
-  base: '/staline.online',
+  //base: '/staline.online',
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [],
